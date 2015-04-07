c8dd89cedb2122529120ae0d80faf4aaee52310c
Yuan Xue <yuanxue68@gmail.com>
2015-04-06 23:59:25 -0400

    trying different routes

--- Hello_World/app.py
+++ Hello_World/app.py
@@ -1,12 +1,38 @@
 from flask import Flask
 
 app=Flask(__name__);
+app.config["DEBUG"]=True
 
 @app.route("/")
 @app.route("/hello")
 
 def hello_world():
-	return "Hello, World"
+	return "Hello, World!!!"
 
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value+1
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value+1
+	return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return"correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower()=="michael":
+		return "Hello, {}".format(name), 200
+	else:
+		return "Not Found",404
 if __name__=="__main__":
 	app.run()
